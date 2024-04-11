4aaae7eeb20dc2b20614583d5021fe3e508b9a69
LLANNILLO <david.llanilloantolin@iesmiguelherrero.com>
2024-04-11 18:20:16 +0200

    T4_H6 : E5

---
+++ TEMA4/HOJA6/ejercicio5/JS/lanzarDados.js
@@ -0,0 +1,11 @@
+function lanzarDados(lanazamientos) {
+  let puntuacionGanadora = 0;
+  let contador = 0;
+
+  do {
+    console.log("lanzamiento");
+    contador++;
+  } while (contador <= lanazamientos);
+}
+
+lanzarDados(2);
