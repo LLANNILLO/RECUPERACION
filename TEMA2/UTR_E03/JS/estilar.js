b42b9b9f16e86101899529bbaad4489004069adf
LLANNILLO <david.llanilloantolin@iesmiguelherrero.com>
2024-04-16 16:24:37 +0200

    T2_H3

--- TEMA2/UTR_E03/JS/estilar.js
+++ TEMA2/UTR_E03/JS/estilar.js
@@ -3,7 +3,6 @@ window.onload = () => {
   const opcionesDiv = document.getElementById("segundoDiv");
   // Obtener referencia al contenedor padre
   const contenedor = document.querySelector(".contenedor");
-  console.log(contenedor);
 
   const positionSelect = document.getElementById("position");
   const topInput = document.getElementById("top_valor");
@@ -12,10 +11,10 @@ window.onload = () => {
 
   // Función para actualizar las clases y estilos del div
   function actualizarClasesDiv() {
-    const positionValue = positionSelect.value;
+    const positionValue = positionSelect.value; //Valor del position
     const topValue = topInput.value + "px"; // Convertir a píxeles
     const leftValue = leftInput.value + "px"; // Convertir a píxeles
-    const overflowValue = overflowSelect.value;
+    const overflowValue = overflowSelect.value; //Valor del overflow
 
     // Aplicar clases al div según los valores seleccionados
     opcionesDiv.style.position = positionValue;
