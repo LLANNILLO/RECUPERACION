22df950c28c3a430a5db5a4167c72e1e7468930e
LLANNILLO <david.llanilloantolin@iesmiguelherrero.com>
2024-04-18 17:29:20 +0200

    T4_O1 : E5,6

--- TEMA4/OBJETO1/ejercicio5/JS/Ejercicio.js
+++ TEMA4/OBJETO1/ejercicio5/JS/Ejercicio.js
@@ -1,31 +1,33 @@
-var Director = new Miembro("Alfonso Cuarón", new Date("2020-07-23"), true);
-var Profesor = new Profesor("Enrique Iglesias", new Date("6-6-6"), false, 2000);
+var director = new Miembro("Alfonso Cuarón", new Date("2020-07-23"), true);
+var profesor = new Profesor("Enrique Iglesias", new Date("6-6-6"), false, 20);
+var alumno = new Alumno("Stephen Hawking", new Date("2002-02-20"), true, 12);
 
-var Alumno = new Alumno("Stephen Hawking", new Date("2002-02-20"), true, 1200);
-
-let arrayMiembros = [Director, Profesor, Alumno];
+let arrayMiembros = [director, profesor, alumno];
 
 arrayMiembros.forEach((miembro) => {
   if (miembro instanceof Miembro) {
     console.log(`
     Nombre: ${miembro.nombre}\n
-    Fecha: ${miembro.fecha.toString()},
+    Fecha: ${miembro.alta.toString()},
     Estado: ${miembro.estado}
     Asignaturas: ${miembro.numeroAlumnos}
     `);
+    miembro.cobrar();
   } else if (miembro instanceof Profesor) {
     console.log(`
     Nombre: ${miembro.nombre}\n
-    Fecha: ${miembro.fecha.toString()},
+    Fecha: ${miembro.alta.toString()},
     Estado: ${miembro.estado}
     Alumnons: ${miembro.numeroAsignaturas}
     `);
+    miembro.cobrar();
   } else {
     console.log(`
         Nombre: ${miembro.nombre}\n
-        Fecha: ${miembro.fecha.toString()},
+        Fecha: ${miembro.alta.toString()},
         Estado: ${miembro.estado}
         Asignaturas: ${miembro.numeroAsignaturas}
         `);
+    miembro.cobrar();
   }
 });
