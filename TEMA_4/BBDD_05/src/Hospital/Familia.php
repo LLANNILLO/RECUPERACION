7a8ced36882ada8a507c803e7d88481a0d2d0088
LLANNILLO <david.llanilloantolin@iesmiguelherrero.com>
2024-04-22 15:57:45 +0200

    TEMA04_BBDD05

---
+++ TEMA_4/BBDD_05/src/Hospital/Familia.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BD5\Hospital;
+
+use BD5\Enum\Turno;
+
+class Familia extends Medico
+{
+
+    protected int $num_pacientes;
+
+    public function __construct(string $codigo, string $nombre, int $edad, Turno $turno, int $num_pacientes)
+    {
+        parent::__construct($codigo, $nombre, $edad, $turno);
+
+        $this->num_pacientes = $num_pacientes;
+    }
+
+
+    //metodos getter Medico
+    public function getPacientes()
+    {
+        return $this->num_pacientes;
+    }
+
+    //metodos setter Medico
+
+    public function setPacientes(int $pacientes)
+    {
+        $this->num_pacientes = $pacientes;
+    }
+
+    //metodo toString Medico
+
+    public function toString()
+    {
+
+        $resultado = parent::toString();
+
+        $resultado .= "<p>Numero de pacientes; {$this->getPacientes()}</p>";
+
+        return $resultado;
+    }
+}
