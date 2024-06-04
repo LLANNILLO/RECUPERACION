753d68acf60f488c3026c6641520f80c180478d3
LLANNILLO <david.llanilloantolin@iesmiguelherrero.com>
2024-06-04 09:42:02 +0200

    T5_H3 : E3

---
+++ TEMA5/HOJA3/ejercicio3/JS/posicion.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", () => {
+  const coordenadas = document.getElementById("coordenadasRaton");
+
+  document.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+  });
+  document.addEventListener("mousemove", (event) => {
+    coordenadasActuales(event);
+  });
+
+  document.addEventListener("mouseup", (event) => {
+    document.getElementById("evento").textContent = event.target.value;
+  });
+
+  function coordenadasActuales(event) {
+    let x = event.clientX;
+    let y = event.clientY;
+    document.getElementById("X").textContent = x;
+    document.getElementById("Y").textContent = y;
+
+    coordenadas.style.visibility = "visible";
+    coordenadas.style.left = x + 5 + "px";
+    coordenadas.style.top = y + 10 + "px";
+  }
+
+  function pulsarRaton(event) {}
+});
