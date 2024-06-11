456627b3239e77077ec9036c00acc52bdea68751
LLANNILLO <david.llanilloantolin@iesmiguelherrero.com>
2024-06-11 18:20:54 +0200

    comienzo con el index y el style

--- TEMA5/HOJA5/JS/class/index.js
+++ TEMA5/HOJA5/JS/class/index.js
@@ -1,6 +1,24 @@
 let electrodomestico = new Television(0, "Negro", "A", 200, 50, true);
 
 window.addEventListener("load", () => {
-  console.log(electrodomestico);
-  console.log(electrodomestico.precioFinal());
+  const selectorElectrodomestico = document.getElementById(
+    "electrodomesticoAgregar"
+  );
+
+  const lavadoraDiv = document.querySelector(".lavadora");
+  const televisorDiv = document.querySelector(".televisor");
+
+  let anterior;
+  selectorElectrodomestico.addEventListener("change", () => {
+    if (anterior) {
+      anterior.removeAttribute("style");
+    }
+    if ((selectorElectrodomestico.value = "lavadora")) {
+      lavadoraDiv.style.display = "block";
+      anterior = lavadoraDiv;
+    } else {
+      televisorDiv.style.display = "block";
+      anterior = televisorDiv;
+    }
+  });
 });
