296540fea04997cb7979f55cf423116bd1c4c91f
LLANNILLO <david.llanilloantolin@iesmiguelherrero.com>
2024-06-15 00:22:29 +0200

    FINALIZADA LA GALERIA

--- TEMA_2/UTR_E09/js/index.js
+++ TEMA_2/UTR_E09/js/index.js
@@ -1,23 +1,83 @@
 window.addEventListener("load", () => {
   $imagenes = document.querySelectorAll("[data-image]");
-  console.log($imagenes);
-  $anteriorImagen = document.getElementById("anteriorImagen");
-  $siguienteImagen = document.getElementById("siguienteImagen");
-  $cerrarImagen = document.getElementById("cerrarImagen");
 
+  let imagenSeleccionada;
+  $contenedorImagenesDiv = document.querySelector(".contenedor-imagenes");
+  $detallesDiv = document.querySelector(".detalles");
+
+  //por cada ficha añadir el evento de actualizarInformacion
+  $imagenes.forEach((elemento) => {
+    elemento.addEventListener("click", () => {
+      ActualizarInformacion(elemento);
+    });
+  });
+
+  //actualiza y muestra los detalles aside
   function ActualizarInformacion(data) {
-    $imagenACargar = data.querySelector("img");
-    $EnlaceACargar = data.querySelector("a");
-    $DescripcionACargar = data.querySelector("span").parentNode.textContent;
+    $enlaceACargar = data.querySelector("a").href;
+    $imagenACargar = data.querySelector("img").src;
+    $descripcionACargar = data.querySelector("#descripcion").textContent;
     $tituloDetalleAnchor = document.getElementById("tituloDetalle");
-    $tituloDetalleAnchor = document.getElementById("imagen-detalles");
-    $tituloDetalleAnchor = document.getElementById("descripcionDetalle");
+    $imagenDetalleSrc = document.getElementById("imagenDetalle");
+    $descripcionDetalleDiv = document.getElementById("descripcionDetalle");
+    imagenSeleccionada = data.dataset.image;
+    $tituloDetalleAnchor.href = $enlaceACargar;
+    $imagenDetalleSrc.src = $imagenACargar;
+    $descripcionDetalleDiv.textContent = $descripcionACargar;
+
+    //solo se aplica si contiene la clase que impide que se vea el detalles
+    if ($detallesDiv.classList.contains("desaparecen-detalles")) {
+      abrirDetalles();
+    }
   }
 
-  $cerrarImagen.addEventListener("click", () => {
-    $contenedorImagenesDiv = document.querySelector(".contenedor-imagenes");
-    $detallesDiv = document.querySelector(".detalles");
+  //funcion para abrir el Detalles
+  function abrirDetalles() {
+    $contenedorImagenesDiv.classList.remove("aumentar-tamaño");
+    $detallesDiv.classList.remove("desaparecen-detalles");
+  }
+
+  //evento para la imagen anterior
+  $anteriorImagen = document.getElementById("anteriorImagen");
+
+  $anteriorImagen.addEventListener("click", () => {
+    anteriorImagen();
+  });
+
+  function anteriorImagen() {
+    if (imagenSeleccionada > 0) {
+      let elementoAnterior = document.querySelector(
+        `[data-image='${--imagenSeleccionada}']`
+      );
+
+      ActualizarInformacion(elementoAnterior);
+    }
+  }
+
+  //evento para la imagen siguiente
+  $siguienteImagen = document.getElementById("siguienteImagen");
+
+  $siguienteImagen.addEventListener("click", () => {
+    siguienteImagen();
+  });
+
+  function siguienteImagen() {
+    if (imagenSeleccionada < 19) {
+      let elementoSiguiente = document.querySelector(
+        `[data-image='${++imagenSeleccionada}']`
+      );
+
+      ActualizarInformacion(elementoSiguiente);
+    }
+  }
+  //evento para cerrar el detalles
+  $cerrarDetalles = document.getElementById("cerrarImagen");
+  $cerrarDetalles.addEventListener("click", () => {
+    cerrarDetalles();
+  });
+
+  function cerrarDetalles() {
     $contenedorImagenesDiv.classList.add("aumentar-tamaño");
     $detallesDiv.classList.add("desaparecen-detalles");
-  });
+  }
 });
